e8c3ac2afce36f959e84a8e1186ef174671e33b3
Bernardo <up202108842@up.pt>
2023-04-22 17:02:37 +0100

    merged with github

--- src/pages/editProfile.php
+++ src/pages/editProfile.php
@@ -4,71 +4,10 @@ require_once(dirname(__DIR__).'/templates/common.tpt.php');
 require_once(dirname(__DIR__).'/database/connection.php');
 require_once(dirname(__DIR__).'/classes/user.class.php');
 require_once(dirname(__DIR__).'/utils/session.php');
+require_once(dirname(__DIR__).'/templates/profile.tpl.php');
 
 drawHeaderMain();
 drawAside();
-?>
-<main id="EditProfileMain">
-    <header class="HeaderMain">
-        Edit Profile
-    </header>
-    <div id="ChangePhoto">
-        <img src="../docs/images/imagem-do-usuario-com-fundo-preto.png">
-        <button> Change Picture</button>
-    </div>
-    <article>
-        <h2>
-            Account Information
-        </h2>
-        <form>
-            <div>
-                Name:
-                <label>
-                    <input type="text" name="newName">
-                </label>
-            </div>
-            <div>
-                E-mail:
-                <label>
-                    <input type="text" name="newEmail">
-                </label>
-            </div>
-        </form>
-    </article>
-    <article>
-        <h2>
-            Account Information
-        </h2>
-        <form>
-            <div>
-                Current Password:
-                <label>
-                    <input type="password" name="currentPassword">
-                </label>
-            </div>
-            <div>
-                New Password:
-                <label>
-                    <input type="password" name="newPassword">
-                </label>
-            </div>
-            <div>
-                Confirm Password:
-                <label>
-                    <input type="password" name="confirmPassword">
-                </label>
-            </div>
-        </form>
-    </article>
-    <div id="SubmitEditButtons">
-        <button formaction="../actions/action_editProfile.php" formmethod="post">
-            Submit Changes
-        </button>
-        <button onclick="window.location.href='../pages/profile.php'">
-            Cancel
-        </button>
-    </div>
-</main>
-<?php
+drawEditProfileMain();
 drawFooterMain(); ?>
 
--- src/pages/profile.php
+++ src/pages/profile.php
@@ -3,6 +3,7 @@ require_once(dirname(__DIR__).'/templates/common.tpt.php');
 require_once(dirname(__DIR__).'/database/connection.php');
 require_once(dirname(__DIR__).'/classes/user.class.php');
 require_once(dirname(__DIR__).'/utils/session.php');
+require_once(dirname(__DIR__).'/templates/profile.tpl.php');
 
 $session = new Session();
 if(!$session->isLoggedIn()) die(header('Location: ../pages/login.php'));
