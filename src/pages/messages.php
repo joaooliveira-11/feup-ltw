7ef70397090637a7721095319c1a1abc18831adc
Bernardo <up202108842@up.pt>
2023-05-19 19:22:04 +0100

    Use answer from faqs done

--- src/pages/messages.php
+++ src/pages/messages.php
@@ -3,16 +3,25 @@
 require_once(dirname(__DIR__).'/templates/common.tpt.php');
 require_once(dirname(__DIR__).'/templates/messages.tpl.php');
 require_once(dirname(__DIR__).'/utils/session.php');
+require_once(dirname(__DIR__).'/database/connection.php');
+require_once(dirname(__DIR__).'/classes/ticket.class.php');
 
 $session = new Session();
 if(!$session->isLoggedIn()) die(header('Location: ../pages/login.php'));
 
+$db = getDatabaseConnection();
+
 $idUser = $session->getId();
 $ticket_id = intval($_SESSION['Ticket']);
-unset($_SESSION['Ticket']);
+$ticket = Ticket::getTicketFromId($db,$ticket_id);
+$resolve = $idUser===$ticket->getResolve();
+$value = null;
+if($_POST['faqQuestion'] && $_POST['faqAnswer']){
+    $value = "From Question on FAQ: " . $_POST['faqQuestion'] . " \n\n Answer: " . $_POST['faqAnswer'];
+}
 
 drawHeaderMain("messages.js");
 drawAside();
-drawMessages($ticket_id, $idUser);
+drawMessages($ticket_id, $idUser, $resolve, $value);
 drawFooterMain(); 
 ?>
---
+++ src/pages/useAnswerFromFAQ.php
@@ -0,0 +1,16 @@
+<?php
+require_once(dirname(__DIR__).'/templates/faq.tpt.php');
+require_once(dirname(__DIR__).'/templates/common.tpt.php');
+require_once(dirname(__DIR__).'/utils/session.php');
+require_once(dirname(__DIR__).'/database/connection.php');
+
+$db = getDatabaseConnection();
+$stmt = $db->prepare("Select * From FAQ");
+$stmt->execute();
+
+drawHeaderMain();
+drawAside();
+drawFAQ($stmt, true);
+drawFooterMain();
+
+?>
